b3686e14abaa0bf452d878c42cfada7b47be4168
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2022-04-03 10:35:09 +0700

    Init weapon module

---
+++ src/server/Weapon.lua
@@ -0,0 +1,5 @@
+local playerServices = game:GetService("Players")
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local hitRemote = replicatedStorage.Events.Hit
+
+local replicateRemote = replicatedStorage.Events.Replicate
