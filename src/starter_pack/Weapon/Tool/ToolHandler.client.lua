b083a0eafe58cee3a3e06c0c39d4dd687f8e71b2
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2022-04-03 13:13:05 +0700

    Udpate ToolHandler

--- src/starter_pack/Weapon/Tool/ToolHandler.client.lua
+++ src/starter_pack/Weapon/Tool/ToolHandler.client.lua
@@ -16,11 +16,99 @@ local gunSettings = require(tool:WaitForChild("Settings"))
 local equipped = false;
 
 
+local ignoreList = {char, workspace.Effects}
+local debrisService = game:GetService("Debris")
+
+-- Equip Detection
 tool.Equipped:Connect(function()
     equipped = true;
 end)
 
 tool.Unequipped:Connect(function()
     equipped = false;
-    
 end)
+
+
+local function castRay()
+	local origin = firePoint.Position
+	local direction = (mouse.Hit.p - firePoint.Position).Unit
+	direction = direction * gunSettings.range
+
+	local ray = Ray.new(origin, direction)
+	--local hit, pos = workspace:FindPartOnRayWithIgnoreList(ray, ignoreList)
+
+
+    local raycastParams = RaycastParams.new()
+    raycastParams.FilterType = ignoreList
+    raycastParams.FilterDescendantsInstances = {workspace.Model}
+    raycastParams.IgnoreWater = true
+
+    local hit, pos = workspace:Raycast(ray, direction, raycastParams)
+
+
+	replicatedStorage.Replicate:FireServer(tool, origin, pos)
+	local visual = Instance.new("Part")
+	local length = (pos - origin).Magnitude
+	visual.Anchored = true
+	visual.CanCollide = false
+	visual.Material = Enum.Material.Neon
+	visual.Color = gunSettings.rayColor
+	visual.Size = Vector3.new(gunSettings.raySize.X, gunSettings.raySize.Y, length)
+	visual.CFrame = CFrame.new(origin, pos) * CFrame.new(0,0,-length/2)
+	visual.Parent = workspace.Effects
+	debris:AddItem(visual, gunSettings.debrisTime)
+
+	return hit, pos, direction, origin
+end
+
+local function gunEffects()
+	for _, effect in pairs(firePoint:GetChildren()) do
+		if effect:IsA("ParticleEmitter") then
+			effect:Emit(50)
+		end
+		
+		if effect:IsA("Sound") then
+			effect:Play()
+		end
+	end
+end
+
+
+local doFire = false
+
+local function fire()
+	local waitTime = 60/gunSettings.rateOfFire
+
+	repeat
+		if equipped and not tool.Debounce.Value then
+			tool.Debounce.Value = true
+
+			delay(waitTime, function()
+				tool.Debounce.Value = false
+			end)
+
+			gunEffects()
+			local hit, pos, direction, origin = castRay()
+
+			if hit then
+				local relCFrame = hit.CFrame:Inverse() * CFrame.new(pos)
+				hitRemote:FireServer(tool, hit, direction, origin, relCFrame)
+			end			
+		end
+		wait(waitTime)
+
+	until not equipped or not doFire or gunSettings.fireMode ~= "AUTO"
+end
+
+
+mouse.Button1Down:Connect(function()
+	doFire = true
+	if char.Humanoid.Health > 0 then
+		fire()
+	end
+end)
+
+mouse.Button1Up:Connect(function()
+	doFire = false
+end)
+
